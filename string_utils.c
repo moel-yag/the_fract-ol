05c591f8d0ba9e45e710244a333da3b94e787079
moel-yag <moel-yag@student.1337.ma>
2025-03-17 15:18:57 +0000

    sinal touch

--- string_utils.c
+++ string_utils.c
@@ -6,58 +6,34 @@
 /*   By: moel-yag <moel-yag@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:20:07 by moel-yag          #+#    #+#             */
-/*   Updated: 2025/03/17 14:54:20 by moel-yag         ###   ########.fr       */
+/*   Updated: 2025/03/17 15:17:19 by moel-yag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include "minilibx-linux/mlx.h"
 
-static void	skip_whitespace(char **s)
+int	is_valid_number(char *str)
 {
-	while ((**s >= 9 && **s <= 13) || **s == ' ')
-		(*s)++;
-}
+	int i;
 
-static int	check_digits_and_dots(char **s, int *has_dot)
-{
-	int	has_digit;
-
-	has_digit = 0;
-	while (**s)
-	{
-		if (**s == '.')
-		{
-			if (*has_dot || !has_digit)
-				return (0);
-			*has_dot = 1;
-		}
-		else if (**s >= '0' && **s <= '9')
-			has_digit = 1;
-		else
-			break ;
-		(*s)++;
-	}
-	return (has_digit);
-}
-
-int	is_valid_number(char *s)
-{
-	int		has_digit;
-	int		has_dot;
-	char	*ptr;
-
-	ptr = s;
-	has_digit = 0;
-	has_dot = 0;
-	skip_whitespace(&ptr);
-	if (*ptr == '+' || *ptr == '-')
-		ptr++;
-	skip_whitespace(&ptr);
-	has_digit = check_digits_and_dots(&ptr, &has_dot);
-	skip_whitespace(&ptr);
-	*ptr = '\0';
-	return (has_digit);
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+		i++;
+	if (!str[i] && i > 0 && (str[i - 1] >= '0' && str[i - 1] <= '9'))
+		return (1);
+	else if (str[i] != '.')
+		return (0);
+	i++;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+		i++;
+	if (!str[i] && i > 0 && (str[i - 1] >= '0' && str[i - 1] <= '9'))
+		return (1);
+	else
+		return (0);
+	return(0);
 }
 
 static int	helper_func2(char *s, double *pow, double *fractional_part)
